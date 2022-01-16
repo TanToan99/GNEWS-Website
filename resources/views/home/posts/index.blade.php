51b623bc168bfd877895404f3a72c2fc5e1c6e7e
github.nqvnlc <locnvgcd18350@fpt.edu.vn>
2022-01-17 00:26:23 +0700

    fix css homepage

--- resources/views/home/posts/index.blade.php
+++ resources/views/home/posts/index.blade.php
@@ -1,15 +1,8 @@
 @extends('layouts.app')
 
-@section('title','DSC | Forum')
-
-@section('header')
-@include('shared.header')
+@section('custom-css')
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 @endsection
-
-@section('header')
-@include('shared.header')
-@endsection
-
 @section('content')
 <div class="section section-basic">
     <div class="container">
--- resources/views/home/posts/post.blade.php
+++ resources/views/home/posts/post.blade.php
@@ -1,27 +1,22 @@
 @extends('layouts.app')
 
 
-@section('header')
-<div class="page-header header-filter" data-parallax="true" style="background-image: url('{{ $post->thumbnail->getUrl() }}');">
+@section('content')
+
+<div class="section section-basic">
     <div class="container">
         <div class="row">
+            <img src="{{ $post->thumbnail->getUrl() }}" alt="">
             <div class="col-md-12 ml-auto mr-auto">
                 <div class="brand">
                     <h2 style="color: white;">{{ $post->title }}</h2>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
-@endsection
-
-@section('content')
-<div class="section section-basic">
-    <div class="container">
-        {!! $post->content !!}
-        <div class="text-right">
-            <small class="text-muted">{{ $post->author->name }} </small><br>
-            <small class="text-muted"> Posted at: {{ $post->created_at }}</small>
+            {!! $post->content !!}
+            <div class="text-right">
+                <small class="text-muted">{{ $post->author->name }} </small><br>
+                <small class="text-muted"> Posted at: {{ $post->created_at }}</small>
+            </div>
         </div>
     </div>
 </div>
