0b4ce2a368b6a4fc0b58f97ecc31d475912b8a2f
TanToan99 <tantoan1909@gmail.com>
2022-01-18 15:34:11 +0700

    favicon fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
 
     <!-- favicon -->
-    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon.png">
 
     <link rel="stylesheet" href="/css/styles.css" />
     @yield('custom-css')
